e18fac3905a6a5c53a7dff3473c32f81bc5d34c8
cpnvbenoit <47849605+cpnvbenoit@users.noreply.github.com>
2019-11-21 11:54:24 +0100

    add ExoDate

---
+++ ExoDate/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Created By PhpStorm
+ * User: benoit.pierrehumbert
+ * Date: 21.11.2019
+ * Time: 11:36
+ */
+echo "<ol>";
+echo "<li>".date("l d F o")."</li>";
+echo "<li>".date("M d S o")."</li>";
+echo "<li>".date("d/m/y h:i a")."</li>";
+echo "<li>".date("d M o, h:i:s")."</li>";
+echo "<li>".date("D, d M o, h:i:s O")."</li>";
+echo "</ol>";
+?>
+
+</body>
+</html>
